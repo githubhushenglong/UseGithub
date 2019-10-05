52f5f49a2e7b327ce5b3d9730f07d6cf169cf32f
githubhushenglong <1842742426@qq.com>
2019-10-05 20:53:59 +0800

    2019/10/05/last

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #git pull
 #同步到本地
 
-git status
+#git status
 
 git add .
 #(or)git add newFile
 
-git commit -m "2019/10/05/添加SH"
+git commit -m "2019/10/05/last"
 #git commit -m "本次更新说明"
 
 git push origin master
